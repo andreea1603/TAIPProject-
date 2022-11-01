18123d0d7750fc73ff4a3992bfc1ca4068619c75
andreihaivas6 <andreihaivas006@gmail.com>
2022-11-02 00:42:30 +0200

    Added MriScans Service & Repo Tests

--- NeuroDiagnosis/src/main/java/com/example/neurodiagnosis/application/service/mri/MriScansService.java
+++ NeuroDiagnosis/src/main/java/com/example/neurodiagnosis/application/service/mri/MriScansService.java
@@ -2,15 +2,22 @@ package com.example.neurodiagnosis.application.service.mri;
 
 import com.example.neurodiagnosis.domain.entities.Mri;
 import com.example.neurodiagnosis.application.interfaces.repositories.IMriScansRepository;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
 
 import java.util.Date;
 import java.util.UUID;
 
 public class MriScansService implements IMriScansService {
-    private IMriScansRepository mriScansRepository;
+    private final IMriScansRepository mriScansRepository;
+
+    @Inject
+    public MriScansService(@Named("mriScansRepository") IMriScansRepository mriScansRepository) {
+        this.mriScansRepository = mriScansRepository;
+    }
 
     @Override
     public Mri submitMriScan(UUID usedId, Date date, String url, Date expiresAt, Date signedAt, String mineType) {
-        return null;
+        return mriScansRepository.addNewScanEntry(usedId, date, url, expiresAt, signedAt, mineType);
     }
 }
--- NeuroDiagnosis/src/main/java/com/example/neurodiagnosis/infrastructure/repositories/MriScansRepository.java
+++ NeuroDiagnosis/src/main/java/com/example/neurodiagnosis/infrastructure/repositories/MriScansRepository.java
@@ -3,13 +3,20 @@ package com.example.neurodiagnosis.infrastructure.repositories;
 import com.example.neurodiagnosis.application.interfaces.repositories.IMriScansRepository;
 import com.example.neurodiagnosis.application.service.database.Database;
 import com.example.neurodiagnosis.domain.entities.Mri;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.UUID;
 
-public class MriScansRepository implements IMriScansRepository {
+@NoArgsConstructor
+@Named("mriScansRepository")
+@SessionScoped
+public class MriScansRepository implements IMriScansRepository, Serializable {
     @Override
     public Mri addNewScanEntry(UUID usedId, Date date, String url, Date expiresAt, Date signedAt, String mineType) {
         Mri mri = new Mri();
