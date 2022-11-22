221403682af2517db39aa8a66d0b94729c96f6a7
StratianuBianca <bianca.stratianu@info.uaic.ro>
2022-11-22 19:22:01 +0200

    Optional MOP

---
+++ NeuroDiagnosis/src/main/java/com/example/neurodiagnosis/verification/mop/monitor2/Test.java
@@ -0,0 +1,10 @@
+package com.example.neurodiagnosis.verification.mop.monitor2;
+
+import java.util.Optional;
+
+public class Test {
+    public static void main(String[] args) {
+        Optional<Integer> opt = Optional.of(10);
+        System.out.println(opt.get());
+    }
+}
