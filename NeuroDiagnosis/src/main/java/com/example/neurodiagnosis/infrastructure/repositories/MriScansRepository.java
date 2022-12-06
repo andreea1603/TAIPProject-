5fcda68f0c2bf7816d47b8bddfd51e846f6adfc5
andreea1603 <andreeaciocan2000@yahoo.ro>
2022-12-06 22:14:32 +0200

    modify java post to receive the response from ml

--- NeuroDiagnosis/src/main/java/com/example/neurodiagnosis/infrastructure/repositories/MriScansRepository.java
+++ NeuroDiagnosis/src/main/java/com/example/neurodiagnosis/infrastructure/repositories/MriScansRepository.java
@@ -9,9 +9,11 @@ import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONObject;
 
 import javax.imageio.ImageIO;
@@ -40,9 +42,7 @@ public class MriScansRepository extends BaseRepository
 
         byte[] bytes = Files.readAllBytes(photo.toPath());
         getMlResults(bytes);
-//
-//        for(int i=0; i<bytes.length; i++)
-//            System.out.println(bytes[i]);
+
         try {
             mri.setImage(Files.readAllBytes(photo.toPath()));
         }catch (Exception e){
@@ -67,22 +67,26 @@ public class MriScansRepository extends BaseRepository
     @Override
     public void getMlResults(byte[] picture) throws IOException {
         try {
-            //picture = scale(picture, 48, 48);
             String encoded = Base64.getEncoder().encodeToString(picture);
             System.out.println(encoded);
+
             String url1 = "http://127.0.0.1:5000/getAnalysis";
 
-            String body = new JSONObject()
-                    .put("imageAsBase64",encoded).toString();
+
+            JSONObject body = new JSONObject();
+            body.put("imageAsBase64", encoded);
+            System.out.println(body);
             HttpClient httpClient = new DefaultHttpClient();
             HttpPost httpPost = new HttpPost(url1);
             httpPost.setHeader("Content-type", "application/json");
             try {
-                StringEntity stringEntity = new StringEntity(body);
+                StringEntity stringEntity = new StringEntity(body.toString());
                 httpPost.getRequestLine();
                 httpPost.setEntity(stringEntity);
 
-                httpClient.execute(httpPost);
+                var response =httpClient.execute(httpPost);
+                var statistics = EntityUtils.toString(response.getEntity());
+
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
@@ -91,21 +95,5 @@ public class MriScansRepository extends BaseRepository
         catch (Exception e){
             System.out.println(e);
         }
-    }
-    public byte[] scale(byte[] fileData, int width, int height) {
-        ByteArrayInputStream in = new ByteArrayInputStream(fileData);
-        try {
-            BufferedImage img = ImageIO.read(in);
-            Image scaledImage = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-            BufferedImage imageBuff = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-            imageBuff.getGraphics().drawImage(scaledImage, 0, 0, new Color(0,0,0), null);
-
-            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-
-            return buffer.toByteArray();
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-        return null;
     }
 }
